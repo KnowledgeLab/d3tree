1abb5d79d01e81274d4fafa0eb93571688a6744f
Jonathan Jin <jjin082693@gmail.com>
2014-11-02 12:01:37 -0600

    Revert "some stuff working??????"
    This reverts commit 75d80b326c576ff9127d0c19957fd02c71f28ee1.

--- js/visualization.js
+++ js/visualization.js
@@ -1,118 +1,16 @@
-"use strict";
+const treeFile = "tree1.json";
+const entryPointSelector = "body";
 
-var treeFile = "tree1.json";
-var entryPointSelector = "body";
+var margin    = {top: 30, right: 20, bottom: 30, left: 20};
+var width     = 960 - margin.left - margin.right;
+var barHeight = 20;
+var barWidth  = width * 0.8;
 
 var i         = 0;
+
 var displayTree = new d3Tree(treeFile);
 
+var diagonal = d3.svg.diagonal()
+    .projection(function(d) { return [d.y, d.x]; });
+
 displayTree.insertInto(entryPointSelector);
-
-function heightAdjust () {
-  var nodes = displayTree.getNodes();
-  var height = Math.max(500, nodes.length * d3Tree.barHeight + d3Tree.margin.top + d3Tree.margin.bottom);
-
-  d3.select("svg").transition()
-    .duration(d3Tree.duration)
-    .attr("height", height);
-
-  d3.select(self.frameElement).transition()
-    .duration(d3Tree.duration)
-    .style("height", height + "px");
-}
-
-function update(source) {
-  // Compute the flattened node list. TODO use d3.layout.hierarchy.
-  var nodes = displayTree.getNodes();
-
-  var height = Math.max(500, nodes.length * d3Tree.barHeight + d3Tree.margin.top + d3Tree.margin.bottom);
-
-  heightAdjust();
-  
-  // Compute the "layout".
-  nodes.forEach(function (n, i) {
-    n.x = i * d3Tree.barHeight;
-  });
-
-  // Update the nodes…
-  var node = displayTree.svg.selectAll("g.node")
-    .data(nodes, function (d) { return d.id || (d.id = ++i); });
-
-  var nodeEnter = node
-    .enter()
-    .append("g")
-    .attr("class", "node")
-    .attr("transform", function (d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
-    .style("opacity", 1e-6);
-
-  // Enter any new nodes at the parent's previous position.
-  nodeEnter.append("rect")
-    .attr("y", -d3Tree.barHeight / 2)
-    .attr("height", d3Tree.barHeight)
-    .attr("width", d3Tree.barWidth)
-    .style("fill", d3Tree.getNodeColor)
-    .on("click", d3Tree.toggleChildren);
-
-  nodeEnter.append("text")
-    .attr("dy", 3.5)
-    .attr("dx", 5.5)
-    .text(function (d) { return d.name; });
-
-  // Transition nodes to their new position.
-  nodeEnter.transition()
-    .duration(d3Tree.duration)
-    .attr("transform", function (d) { return "translate(" + d.y + "," + d.x + ")"; })
-    .style("opacity", 1);
-
-  node.transition()
-    .duration(d3Tree.duration)
-    .attr("transform", function (d) { return "translate(" + d.y + "," + d.x + ")"; })
-    .style("opacity", 1)
-    .select("rect")
-    .style("fill", d3Tree.getNodeColor);
-
-  // Transition exiting nodes to the parent's new position.
-  node.exit().transition()
-    .duration(d3Tree.duration)
-    .attr("transform", function (d) {
-      return "translate(" + source.y + "," + source.x + ")";
-    })
-    .style("opacity", 1e-6)
-    .remove();
-
-  // Update the links…
-  var link = displayTree.svg.selectAll("path.link")
-    .data(displayTree.tree.links(displayTree.getNodes()), function (d) { return d.target.id; });
-
-
-  // Enter any new links at the parent's previous position.
-  link.enter().insert("path", "g")
-    .attr("class", "link")
-    .attr("d", function (d) {
-      var o = {x: source.x0, y: source.y0};
-      return d3Tree.diagonal({source: o, target: o});
-    })
-    .transition()
-    .duration(d3Tree.duration)
-    .attr("d", d3Tree.diagonal);
-
-  // Transition links to their new position.
-  link.transition()
-    .duration(d3Tree.duration)
-    .attr("d", d3Tree.diagonal);
-
-  // Transition exiting nodes to the parent's new position.
-  link.exit().transition()
-    .duration(d3Tree.duration)
-    .attr("d", function (d) {
-      var o = {x: source.x, y: source.y};
-      return d3Tree.diagonal({source: o, target: o});
-    })
-    .remove();
-
-  // Stash the old positions for transition.
-  nodes.forEach(function (d) {
-    d.x0 = d.x;
-    d.y0 = d.y;
-  });
-};
